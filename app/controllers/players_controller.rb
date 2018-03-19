e4948c3b1b7e5e625e0cfefad7260c562098bee9
Unknown <jarb95@gmail.com>
2018-03-18 23:55:49 -0600

    Add new create method and added player resource to routes

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,18 @@
 class PlayersController < ApplicationController
+	def new
+  		
+  	end
+
+  	def create
+		render plain: params[:player].inspect
+		@player = Player.new(player_params)
+
+		@player.save
+		# redirect_to @player
+  	end
+
+  	private
+		def player_params
+			params.require(:player).permit(:name, :age)
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index', as: 'home'
 
-  get 'players/new',	as: 'new_player'
+  resources :players
+  # get 'players/new',	as: 'new_player'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
